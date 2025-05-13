ac6867ba3702a2f6a3ff6f69fdc7ee24dacb1762
Igor <lanin.igor@gmail.com>
2025-05-13 20:28:05 +0200

    publish photo to Telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -10,7 +10,8 @@ def main():
     bot = telegram.Bot(token=tg_bot_token)
 
     chat_id = os.getenv('CHAT_ID')
-    message_id = bot.send_message(chat_id=chat_id, text='Hi, members!')
+    bot.send_message(chat_id=chat_id, text='Hi, members!')
+    bot.send_document(chat_id=chat_id, document=open('images/nasa_apod_7.jpg', 'rb'))
 
 
 if __name__ == '__main__':
